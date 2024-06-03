93169b0e650b028594e32fd9e48b46b3df0db803
Schlauger <gsavatar99.9@gmail.com>
2024-06-03 18:34:29 +0300

    Ready for check

--- workers_service/src/main.py
+++ workers_service/src/main.py
@@ -2,12 +2,11 @@ import numpy as np
 from numpy import random
 import requests
 
-def randomData_jason():
-    ''' Return a JASON file with random 
+def randomData_json():
+    ''' Return a JASON file with random value for each day
     '''
     days=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     value_row = np.random.randint(10, size=7)
-    print(value_row)
     return {i:v for i,v in zip(days, value_row)} #(days_row, value_row)
 
 
@@ -20,25 +19,44 @@ dic_cum={
     "Saturday":0.0,
     "Sunday": 0.0,
     }
+""" A dictionary holds a cumulative sum for each day"""
 
-def updateDays_sum():
+dic_list={
+    "Monday": [],
+    "Tuesday": [],
+    "Wednesday":[],
+    "Thursday":[],
+    "Friday":[],
+    "Saturday":[],
+    "Sunday": [],
+    }
+""" A dictionary holds a list of valu for each day"""
+
+def update_Days_sum():
+    """ Adds the value form the data_json to dic_cum dictionary """
     for key,val in data_json.items():
         dic_cum[key] += val
 
+def update_Days_list():
+    """ Appends the value form the data_json to dic_list dictionary """
+    for key,val in data_json.items():
+        dic_list[key].append(val)
 
 days=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 
+#create a random json 
+data_json = randomData_json()
+#Adds the value form the data_json to dic_cum dictionary
+update_Days_sum()
+# Appends the value form the data_json to dic_list dictionary
+update_Days_list()
 
-data_json = randomData_jason()
-print(data_json)
-updateDays_sum()
-print(dic_cum,'\n\n')
+print("dic_cum",':',dic_cum)
+print("dic_list",':',dic_list)
 
-data_json = randomData_jason()
-print(data_json)
-updateDays_sum()
-print(dic_cum,'\n\n')
+
+
 # Serializing json
 #json_object = json.dumps(dictionary, indent=3)
  
